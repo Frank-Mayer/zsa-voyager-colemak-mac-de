91eb48664fa15d4e15b2090c1c2bb10c17155657
Frank Mayer <git@frank-mayer.io>
2023-10-14 17:50:54 +0200

    update

--- keymap.c
+++ keymap.c
@@ -54,24 +54,20 @@ enum custom_keycodes {
 
 
 
-enum tap_dance_codes {
-  DANCE_0,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_GRAVE,       
-    KC_LGUI,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,                                           KC_J,           KC_L,           KC_U,           KC_Z,           KC_P,           KC_EQUAL,       
-    KC_LALT,        KC_A,           KC_R,           KC_S,           KC_T,           KC_D,                                           KC_H,           KC_N,           KC_E,           KC_I,           KC_O,           KC_RBRACKET,    
-    KC_LCTRL,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_K,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLASH,      
-                                                    LT(1,KC_TAB),   KC_SPACE,                                       KC_ENTER,       MT(MOD_LSFT, KC_BSPACE)
+    KC_LGUI,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,                                           KC_J,           KC_L,           KC_U,           KC_Z,           KC_BSPACE,      KC_EQUAL,       
+    MT(MOD_LALT, KC_TAB),KC_A,           KC_R,           KC_S,           KC_T,           KC_D,                                           KC_H,           KC_N,           KC_E,           KC_I,           KC_O,           KC_RBRACKET,    
+    KC_LCTRL,       KC_Y,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_K,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLASH,      
+                                                    TT(1),          KC_SPACE,                                       KC_ENTER,       KC_LSHIFT
   ),
   [1] = LAYOUT_voyager(
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
+    TO(2),          KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
     KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_AUDIO_VOL_UP,                                KC_TRANSPARENT, KC_TRANSPARENT, KC_LBRACKET,    KC_TRANSPARENT, KC_TRANSPARENT, KC_F12,         
     KC_TRANSPARENT, KC_QUOTE,       KC_TRANSPARENT, KC_MINUS,       KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,                                KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_SCOLON,      KC_F16,         
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_MUTE,                                  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F17,         
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, TD(DANCE_0)
+    KC_TRANSPARENT, RGB_SPD,        RGB_SPI,        RGB_TOG,        RGB_MOD,        KC_AUDIO_MUTE,                                  KC_F22,         KC_F21,         KC_F20,         KC_F19,         KC_F18,         KC_F17,         
+                                                    KC_TRANSPARENT, KC_TAB,                                         KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [2] = LAYOUT_voyager(
     TO(0),          KC_MS_BTN1,     KC_MS_BTN2,     KC_MS_BTN3,     KC_MS_BTN4,     KC_MS_BTN5,                                     KC_MS_BTN6,     KC_MS_BTN7,     KC_MS_BTN8,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -91,7 +87,7 @@ void keyboard_post_init_user(void) {
 }
 
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-    [1] = { {0,0,0}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {0,0,0}, {0,0,0}, {79,255,255}, {79,255,255}, {79,255,255}, {19,255,255}, {0,0,0}, {210,255,255}, {0,0,0}, {210,255,255}, {0,0,0}, {19,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {12,255,255}, {0,0,0}, {0,0,0}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {0,0,0}, {0,0,0}, {210,255,255}, {0,0,0}, {0,0,0}, {114,255,255}, {36,255,255}, {36,255,255}, {36,255,255}, {36,255,255}, {210,255,255}, {114,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {114,255,255}, {0,0,0}, {246,255,255} },
+    [1] = { {246,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {182,255,255}, {0,0,0}, {79,255,255}, {79,255,255}, {79,255,255}, {19,255,255}, {182,255,255}, {210,255,255}, {0,0,0}, {210,255,255}, {0,0,0}, {19,255,255}, {182,255,255}, {50,255,255}, {50,255,255}, {50,255,255}, {50,255,255}, {12,255,255}, {0,0,0}, {0,0,0}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {0,0,0}, {0,0,0}, {210,255,255}, {0,0,0}, {0,0,0}, {114,255,255}, {36,255,255}, {36,255,255}, {36,255,255}, {36,255,255}, {210,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {114,255,255}, {0,0,0}, {0,0,0} },
 
     [2] = { {0,245,245}, {196,255,255}, {196,255,255}, {196,255,255}, {196,255,255}, {196,255,255}, {0,0,0}, {131,255,255}, {145,255,255}, {158,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {196,255,255}, {196,255,255}, {196,255,255}, {196,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {196,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {196,255,255}, {196,255,255}, {196,255,255}, {196,255,255}, {0,0,0}, {0,0,0}, {196,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {196,255,255}, {0,0,0} },
 
@@ -143,58 +139,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-typedef struct {
-    bool is_press_action;
-    uint8_t step;
-} tap;
 
-enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    MORE_TAPS
-};
-
-static tap dance_state[1];
-
-uint8_t dance_step(qk_tap_dance_state_t *state);
-
-uint8_t dance_step(qk_tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
-    } else if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-    }
-    return MORE_TAPS;
-}
-
-
-void dance_0_finished(qk_tap_dance_state_t *state, void *user_data);
-void dance_0_reset(qk_tap_dance_state_t *state, void *user_data);
-
-void dance_0_finished(qk_tap_dance_state_t *state, void *user_data) {
-    dance_state[0].step = dance_step(state);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: layer_move(2); break;
-        case SINGLE_HOLD: register_code16(KC_LSHIFT); break;
-        case DOUBLE_TAP: layer_move(2); break;
-        case DOUBLE_SINGLE_TAP: layer_move(2); break;
-    }
-}
-
-void dance_0_reset(qk_tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
-    switch (dance_state[0].step) {
-        case SINGLE_HOLD: unregister_code16(KC_LSHIFT); break;
-    }
-    dance_state[0].step = 0;
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_0_finished, dance_0_reset),
-};
